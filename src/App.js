465b03e78072c456d724fa055c3eea07d5a55727
Hoang-Nho <vominhhoang312@gmail.com>
2023-03-03 01:09:27 +0700

    --layout basic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Layout } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import routes from "./router/Routers";
@@ -5,7 +7,13 @@ import routes from "./router/Routers";
 function App() {
   return (
     <BrowserRouter>
-      {routes}
+      <Layout>
+        <Header>Đây là header</Header>
+
+        <Content>{routes}</Content>
+
+        <Footer>Đây là footer</Footer>
+      </Layout>
     </BrowserRouter>
   );
 }
